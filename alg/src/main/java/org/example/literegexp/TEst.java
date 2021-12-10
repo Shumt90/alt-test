0293eaee3aee4b2b276bc20eb62caf24322e431c
Шумейко Александр <Avshumeyko@sberbank.ru>
2021-12-10 17:36:25 +0300

    -

---
+++ alg/src/main/java/org/example/literegexp/TEst.java
@@ -0,0 +1,10 @@
+package org.example.literegexp;
+
+import java.util.Arrays;
+
+public class TEst {
+
+  public static void main(String[] args) {
+    System.out.println(Arrays.toString("*..*".split("\\*")));
+  }
+}
---
+++ alg/src/main/java/org/example/literegexp/TestCase.java
@@ -0,0 +1,23 @@
+package org.example.literegexp;
+
+public class TestCase {
+
+    public String regexp;
+    public String value;
+    public boolean isMatch;
+
+    public TestCase(String regexp, String value, boolean isMatch) {
+        this.regexp = regexp;
+        this.value = value;
+        this.isMatch = isMatch;
+    }
+
+    @Override
+    public String toString() {
+        return "TestCase{" +
+                "regexp='" + regexp + '\'' +
+                ", value='" + value + '\'' +
+                ", isMatch=" + isMatch +
+                '}';
+    }
+}
